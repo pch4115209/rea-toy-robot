a99d153705b27a2a172b5e3f83af2e0a32929147
Peter Pan <ppan@forerunner.com.au>
2018-03-20 17:06:03 +1030

    Add tests for Tabletop::class

--- tests/unit/TabletopTest.php
+++ tests/unit/TabletopTest.php
@@ -4,20 +4,37 @@ use PHPUnit\Framework\TestCase;
 
 class TabletopTest extends TestCase
 {
+    private $tabletop;
+
+    /**
+     * setUp() will be called before each test method
+     */
+    public function setUp()
+    {
+        $this->tabletop = new Tabletop(5,5);
+    }
 
     public function testConstructor()
     {
-        $this->assertTrue(true);
+        $this->assertInstanceOf("Tabletop", $this->tabletop);
     }
 
-    public function testIsMovable()
-    {
-
-    }
-
+    /**
+     * @covers Tabletop:isPlaceable
+     */
     public function testIsPlaceale()
     {
+        //Go through every valid coordinates in this 5x5 tabletop
+        for($i = 0; $i < 5; $i++){
+            for($j = 0; $j < 5; $j++){
+                $this->assertTrue($this->tabletop->isPlaceable($i,$j));
+            }
+        }
 
+        $this->assertFalse($this->tabletop->isPlaceable(0,-1));
+        $this->assertFalse($this->tabletop->isPlaceable(-1,0));
+        $this->assertFalse($this->tabletop->isPlaceable(5,0));
+        $this->assertFalse($this->tabletop->isPlaceable(0,5));
     }
 
 }

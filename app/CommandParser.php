a313e3a4e82174d2da349752d954a3051dbdbcbd
Chenghao (Peter) Pan <chenghao.pan@gmail.com>
2018-03-21 19:12:12 +1030

    Add implementation of CommandParser::class and passed all pre-defined tests

--- app/CommandParser.php
+++ app/CommandParser.php
@@ -8,23 +8,41 @@ class CommandParser
     }
 
     public function isValidPlace($line){
+        // Regular Expression
+        $pattern = "/^PLACE\s[0-9]+,[0-9]+,(?:NORTH|EAST|SOUTH|WEST)$/";
 
+        if( preg_match($pattern, trim($line)) === 1 )
+            return true;
+        else
+            return false;
     }
 
     public function isValidMove($line){
-
+        if( trim($line) === 'MOVE' )
+            return true;
+        else
+            return false;
     }
 
     public function isValidLeft($line){
-
+        if( trim($line) === 'LEFT' )
+            return true;
+        else
+            return false;
     }
 
     public function isValidRight($line){
-
+        if( trim($line) === 'RIGHT' )
+            return true;
+        else
+            return false;
     }
 
     public function isValidReport($line){
-
+        if( trim($line) === 'REPORT' )
+            return true;
+        else
+            return false;
     }
 
 
